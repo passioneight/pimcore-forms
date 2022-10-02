68ad850aa5d809a1f9d30408076d7b12be2a2a86
Passioneight <office@passioneight.at>
2022-10-02 15:57:04 +0200

    fix: inherit from correct extension class
    Symfony does not allow to inherit from `ConfigurableExtension` without providing a `Configuration`.

--- src/DependencyInjection/PimcoreFormsExtension.php
+++ src/DependencyInjection/PimcoreFormsExtension.php
@@ -4,16 +4,16 @@ namespace Passioneight\PimcoreForms\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class PimcoreFormsExtension extends ConfigurableExtension
+class PimcoreFormsExtension extends Extension
 {
     /**
      * @inheritDoc
      * @throws \Exception
      */
-    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('config.yaml');
